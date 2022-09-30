47fff0ff1e2e6bd531a4c042b55600adaaa43bd5
Александр Ульченков <youlchenkov@icloud.com>
2022-09-30 18:31:34 +0300

    Поправил.

--- src/Book.java
+++ src/Book.java
@@ -1,13 +1,16 @@
 public class Book {
 
-    private final  Author author;
+    private final String author;
     private final String publisherName; // Название
     private int publishingYear; // Год публикации
 
-    public Book(String publisherName, int publishingYear, Author author) {
+    public Book(String publisherName, int publishingYear, String author) {
         this.author = author;
         this.publisherName = publisherName;
         this.publishingYear = publishingYear;
+    }
+    public String getAuthor() {
+        return this.author;
     }
 
     public String getPublisherName() {
